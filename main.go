9f373d1fde010c81aaba09da74991dcf4a4a5e1a
Antonio Thomacelli <tonnytg@gmail.com>
2021-06-29 14:50:20 -0300

    feat: add time lopp

--- main.go
+++ main.go
@@ -1,33 +1,26 @@
 package main
 
-// Importing fmt and time
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"time"
 )
-import "time"
 
-// Calling main
 func main() {
-
-	// Declaring t in UTC
-	//t := time.Date(2020, 13, 34, 12, 45, 55, 0, time.UTC)
 	startTime := time.Now()
+	SHour, SMin, SSec := startTime.Clock()
+	fmt.Printf("goPomo Start: %v:%v:%v\n", SHour, SMin, SSec)
+	timeToRunning := 25
+	fmt.Printf("Pomodoro Running: %dmin\n", timeToRunning)
 
-	// Calling Clock method
-	StHour, StMin, StSec := startTime.Clock()
-
-	// Prints hours
-	fmt.Printf("Pomodoro Start: %v:%v:%v\n", StHour, StMin, StSec)
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		if scanner.Scan() {
-			EndTime := time.Now()
-			EndHour, EndMin, EndSec := EndTime.Clock()
-			fmt.Printf("Pomodoro Start: %v:%v:%v\n", EndHour, EndMin, EndSec)
+	i := 0
+	for range time.Tick(1 * time.Second) {
+		if i == 1500 {
 			break
 		}
+		i++
 	}
+
+	endTime := time.Now()
+	EHour, EMin, ESec := endTime.Clock()
+	fmt.Printf("goPomo Stop: %v:%v:%v\n", EHour, EMin, ESec)
 }
