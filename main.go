75cc1f6d35cbb0beccc89dbc75583be423ffaf61
Antonio Thomacelli <tonnytg@gmail.com>
2021-07-01 19:02:09 -0300

    feat: add arguments -quant=1 -time=25

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 )
@@ -29,34 +30,45 @@ func GetTime(x *Pomodoro) (Hour, Min, Sec, Loop int) {
 // You can use 25 minutes or custom minutes do you want.
 func LogicPomodoro(x int) error{
 	for i := 0; i < x; i++ {
-		time.Sleep(10 * time.Second)
+		time.Sleep(60 * time.Second)
 	}
 	return nil
 }
+
+var (
+	x Pomodoro
+	PomodoroTime int = 1
+	PomodoroQuantity int = 1
+)
 
 func main() {
 
 	// PomodoroTime it is a Default time of Pomodoro.
 	// It is 25 minutes with 5 minutes with pause for a break.
-	PomodoroTime := 1
-	PomodoroQuantity := 1
-	var x Pomodoro
+	PomodoroTime := flag.Int("time", 25, "Custom Time")
+	PomodoroQuantity := flag.Int("quant", 1, "Pomodoro Quantity")
 
-	for i := 1; i <= PomodoroQuantity; i++ {
-		// GetTime for StartTime
+	// Check some Args different of default
+	flag.Parse()
+
+	fmt.Println("Time:", *PomodoroTime)
+	fmt.Println("Quantity:", *PomodoroQuantity)
+
+	for i := 1; i <= *PomodoroQuantity; i++ {
+		// GetTime Start
 		GetTime(&x)
-		fmt.Printf("Start: %d:%d:%d Pomodoro: %d/%d\n", x.Hour, x.Min, x.Sec, x.Loop, PomodoroQuantity)
+		fmt.Printf("Start: %d:%d:%d Pomodoro: %d/%d\n", x.Hour, x.Min, x.Sec, x.Loop, *PomodoroQuantity)
 
 		// Looping with time sleep
-		err := LogicPomodoro(PomodoroTime)
+		err := LogicPomodoro(*PomodoroTime)
 		if err != nil {
 			fmt.Println("Error", err)
 		}
 
-		// GetTime for EndTime
+		// GetTime End
 		GetTime(&x)
 		x.Count()
-		fmt.Printf("End: %d:%d:%d Pomodoro: %d/%d\n", x.Hour, x.Min, x.Sec, x.Loop, PomodoroQuantity)
+		fmt.Printf("End: %d:%d:%d Pomodoro: %d/%d\n", x.Hour, x.Min, x.Sec, x.Loop, *PomodoroQuantity)
 
 		switch {
 		case i % 5 == 0:
@@ -66,6 +78,5 @@ func main() {
 			fmt.Printf("Take 5 minutes for a cofee!\nPress Enter to continue!\n")
 			fmt.Scanln()
 		}
-
 	}
 }
